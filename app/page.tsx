4f1c7cd8e7435e43d03d1e0566b00c7a6eef722c
Virginie17 <157920288+Virginie17@users.noreply.github.com>
2026-07-08 17:14:33 +0200

    Use new conversion-focused landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,5 @@
-import Navbar from "@/components/landing/Navbar";
-import HeroSection from "@/components/landing/HeroSection";
-import GuideAidantSection from "@/components/landing/GuideAidantSection";
-import TargetChoiceSection from "@/components/landing/TargetChoiceSection";
-import ServicesSection from "@/components/landing/ServicesSection";
-import ProfessionalSection from "@/components/landing/ProfessionalSection";
-import AboutSection from "@/components/landing/AboutSection";
-import BenefitsSection from "@/components/landing/BenefitsSection";
-import AreaSection from "@/components/landing/AreaSection";
-import TestimonialsSection from "@/components/landing/TestimonialsSection";
-import FAQSection from "@/components/landing/FAQSection";
-import ContactSection from "@/components/landing/ContactSection";
-import Footer from "@/components/landing/Footer";
-import PricingSection from "@/components/landing/PricingSection";
+import LandingPage from "@/components/LandingPage";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <Navbar />
-      <HeroSection />
-      <GuideAidantSection />
-      <TargetChoiceSection />
-      <ServicesSection />
-      <ProfessionalSection />
-      <AboutSection />
-      <BenefitsSection />
-      <PricingSection />
-      <AreaSection />
-      <TestimonialsSection />
-      <FAQSection />
-      <ContactSection />
-      <Footer />
-    </main>
-  );
+  return <LandingPage />;
 }
